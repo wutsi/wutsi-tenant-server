8b920011308c2923b11837bc77dadc9c833c0db3
htchepannou <htchepannou@expedia.com>
2021-11-13 12:31:48 -0500

    update depedencies

--- src/test/kotlin/com/wutsi/platform/tenant/endpoint/AbstractSecuredController.kt
+++ src/test/kotlin/com/wutsi/platform/tenant/endpoint/AbstractSecuredController.kt
@@ -1,9 +1,5 @@
 package com.wutsi.platform.tenant.endpoint
 
-import com.auth0.jwt.interfaces.RSAKeyProvider
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.platform.core.security.ApiKeyProvider
 import com.wutsi.platform.core.security.SubjectType
 import com.wutsi.platform.core.security.SubjectType.USER
@@ -17,33 +13,17 @@ import com.wutsi.platform.core.test.TestTracingContext
 import com.wutsi.platform.core.tracing.TracingContext
 import com.wutsi.platform.core.tracing.spring.SpringTracingRequestInterceptor
 import com.wutsi.platform.core.util.URN
-import com.wutsi.platform.security.WutsiSecurityApi
-import com.wutsi.platform.security.dto.GetKeyResponse
-import com.wutsi.platform.security.dto.Key
 import org.junit.jupiter.api.BeforeEach
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.web.client.RestTemplate
-import java.util.Base64
 
 abstract class AbstractSecuredController {
-    private lateinit var keyProvider: RSAKeyProvider
     private lateinit var tracingContext: TracingContext
     private lateinit var apiKeyProvider: ApiKeyProvider
-
-    @MockBean
-    lateinit var securityAPI: WutsiSecurityApi
 
     @BeforeEach
     open fun setUp() {
         tracingContext = TestTracingContext()
         apiKeyProvider = TestApiKeyProvider("00000000-00000000-00000000-00000000")
-        keyProvider = TestRSAKeyProvider()
-
-        val key = Key(
-            algorithm = "RSA",
-            content = Base64.getEncoder().encodeToString(keyProvider.getPublicKeyById("1").encoded)
-        )
-        doReturn(GetKeyResponse(key)).whenever(securityAPI).getKey(any())
     }
 
     protected fun createResTemplate(
@@ -60,7 +40,7 @@ abstract class AbstractSecuredController {
                 name = URN.of("user", subjectId.toString()).value,
                 subjectType = subjectType,
                 scope = scope,
-                keyProvider = keyProvider,
+                keyProvider = TestRSAKeyProvider(),
                 admin = admin
             ).build()
         )
